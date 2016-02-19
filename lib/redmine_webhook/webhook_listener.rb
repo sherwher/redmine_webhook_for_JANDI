7066f3ca56f020d4ce723412a58a0689a89852f1
Joehwang <joehwang.com@gmail.com>
2016-02-19 19:02:38 +0800

    add JANDI Incoming Webhooks api support
    Redmine > project > setting > Webhook
    set JANDI Webhook URL
    done!

--- lib/redmine_webhook/webhook_listener.rb
+++ lib/redmine_webhook/webhook_listener.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module RedmineWebhook
   class WebhookListener < Redmine::Hook::Listener
 
@@ -45,15 +46,50 @@ module RedmineWebhook
     def post(webhook, request_body)
       Thread.start do
         begin
-          Faraday.post do |req|
-            req.url webhook.url
-            req.headers['Content-Type'] = 'application/json'
-            req.body = request_body
-          end
+        
+
+      jandiapi(webhook.url,JSON.parse(request_body))
         rescue => e
           Rails.logger.error e
         end
       end
     end
+def jandiapi(_endpoint,m)
+    
+    project=m["payload"]["issue"]["project"]["name"]
+    action=m["payload"]["action"]
+    subject=m["payload"]["issue"]["subject"]
+    desc=m["payload"]["issue"]["description"]
+    author=(m["payload"]["issue"]["author"].nil?) ? "": m["payload"]["issue"]["author"]["login"]
+    assignee=(m["payload"]["issue"]["assignee"].nil?) ? "not assign": m["payload"]["issue"]["assignee"]["login"]
+    status=m["payload"]["issue"]["status"]["name"]
+    url=m["payload"]["url"]
+      @msgbody = {
+        body: "Redmine - #{project}", #Body text (Required)
+        connectColor: '#FAC11B', #Hex code color of attachment bar
+    connectInfo: [{
+        title: "#{subject}" , #1st attachment area title
+        description: "#{desc}" #1st attachment description
+        },
+        {
+        title: 'assigner', #2nd attachment area title
+        description: "#{assignee}", #2nd attachment description        
+        },
+        {
+        title: "#{status}", #2nd attachment area title
+        imageUrl: "#{url}" #2nd attachment description        
+        }]
+      }.to_json
+      uri = URI(_endpoint)
+      http = Net::HTTP.new(uri.host, 443)
+      request = Net::HTTP::Post.new(uri.request_uri,initheader = {'Content-Type' =>'application/json','Accept' =>'application/vnd.tosslab.jandi-v2+json'})
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request.body = "#{@msgbody}"
+      res = http.request(request)
+      puts res.body     
+
   end
+ 
+ end
 end
